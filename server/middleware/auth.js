67a0e78ab4616c39c2bff8bb486d811d93e09c6b
Astra <abhaypratapsinghwd@gmail.com>
2024-06-16 21:43:12 +0530

    Added Auth Middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,12 +1,28 @@
-const jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
 
-const authMiddleware = (req, res, next) => {
-  if (req.headers.authorization) {
-    const token = req.headers.authorization.split(" ")[1];
-    const decodedData = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    req.userId = decodedData?.id;
+export default function (req, res, next) {
+  const authHeader = req.header("Authorization");
+
+  // Check if Authorization header exists
+  if (!authHeader) {
+    return res.status(401).json({ message: "Authorization denied" });
   }
-  next();
-};
+  // Get token from header
+  const token = authHeader.split(" ")[1];
 
-module.exports = authMiddleware;
+  // Check if token doesn't exist
+  if (!token) {
+    return res.status(401).json({ message: "Authorization denied" });
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Add user from payload
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+}
