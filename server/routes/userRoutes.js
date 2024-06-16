61f50517ca5a9ccb4498c36fea23901d9115e39c
Astra <abhaypratapsinghwd@gmail.com>
2024-06-16 21:46:41 +0530

    Added registerUserController

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,23 @@
 import express from "express";
-import { signIn, signUp } from "../controllers/user.js";
+import {
+  //   fetchUserController,
+  //   forgotPasswordController,
+  //   loginUserController,
+  registerUserController,
+} from "../controllers/userController.js";
+import authMiddleware from "../middleware/auth.js";
+
 const router = express.Router();
 
-router.post("/signIn", signIn);
-router.post("/signUp", signUp);
+router.post("/register", registerUserController);
+// router.post("/login", loginUserController);
+// router.post("/forgotPassword", forgotPasswordController);
+// router.post("/verifyOTP", verifyOTPController);
+// router.post("/resetPassword", resetPasswordController);
+
+// router.get("/", authMiddleware, tokenVerificationController);
+// router.get("/:userName", fetchUserController);
+
+// router.put("/updateUser", authMiddleware, updateUserController);
 
 export default router;
