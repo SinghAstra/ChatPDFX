2bfbd247045677e9cf4ce52c4c93ee756b89c901
Astra <abhaypratapsinghwd@gmail.com>
2024-07-08 14:36:11 +0530

    Fetch User Info Using JWT

--- controllers/userController.js
+++ controllers/userController.js
@@ -138,8 +138,9 @@ export const loginUserController = async (req, res) => {
 
     console.log("token is ", token);
 
-    res.json({ message: "Login successful", username: user.username, token });
+    res.json({ message: "Login successful", token });
   } catch (error) {
+    console.log("error is ", error);
     res.status(500).json({ message: "Error while Logging in user." });
   }
 };
@@ -159,5 +160,27 @@ export const fetchUserInfoUsingEmail = async (req, res) => {
     res.json({ user });
   } catch (error) {
     res.status(500).json({ message: "Error fetching user information" });
+  }
+};
+
+export const fetchUserInfoUsingJWTToken = async (req, res) => {
+  try {
+    const { username } = req.user;
+
+    // Check if username is provided in the query
+    if (!username) {
+      return res.status(400).json({ message: "Username is required" });
+    }
+    // Check if user exists in the database
+    const user = await User.findOne({ username }).select(
+      "username profile email"
+    );
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ user, message: "User Info fetched" });
+  } catch (error) {
+    res.status(500).json({ message: "Error while fetching user Info." });
   }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,11 @@ import express from "express";
 import upload from "../config/multer.js";
 import {
   fetchUserInfoUsingEmail,
+  fetchUserInfoUsingJWTToken,
   loginUserController,
   registerUserController,
 } from "../controllers/userController.js";
+import auth from "../middleware/auth.js";
 import User from "../models/User.js";
 
 const router = express.Router();
@@ -12,6 +14,8 @@ const router = express.Router();
 router.post("/register", upload.single("picture"), registerUserController);
 router.post("/login", loginUserController);
 router.post("/fetch-user-info", fetchUserInfoUsingEmail);
+router.post("/verify-token", auth, fetchUserInfoUsingJWTToken);
+
 router.get("/testing", async (req, res) => {
   const users = await User.find();
 
