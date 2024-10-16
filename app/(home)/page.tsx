a58b4857fe092d2a6d5b6e8a2f44aeb1054664d5
Astra <abhaypratapsinghwd@gmail.com>
2024-10-16 16:10:46 +0530

    Added Hero Section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,11 @@
+import Hero from "@/components/hero";
 import React from "react";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">Home</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <Hero />
+    </div>
   );
 };
 
