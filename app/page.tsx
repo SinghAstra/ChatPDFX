f348910b751c092ba3123890f968a04dd4aec747
Astra <abhaypratapsinghwd@gmail.com>
2025-05-27 17:39:53 +0530

    basic docs upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
-import Navbar from "@/components/(home)/navbar";
-import ChatSection from "./chat-section";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Loader } from "lucide-react";
+import { useState } from "react";
 
-export default function Home() {
+export default function PDFParserClient() {
+  const [status, setStatus] = useState("idle");
+  const [isUploading, setIsUploading] = useState(false);
+
+  const handleClick = async () => {
+    setStatus("Uploading & Parsing...");
+    setIsUploading(true);
+    const res = await fetch("/api/parse-pdf");
+    setIsUploading(false);
+    const data = await res.json();
+    setStatus(data.status === "completed" ? "✅ Done!" : "❌ Failed.");
+    console.log("Final Result:", data);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center gap-10 p-24 background-gradient">
-      <Navbar />
-      <ChatSection />
-    </main>
+    <div className="min-h-screen flex flex-col items-center justify-center gap-2">
+      <Button onClick={handleClick} disabled={isUploading}>
+        {isUploading ? (
+          <>
+            <Loader className="w-2 h-2 animate-spin" /> Wait
+          </>
+        ) : (
+          "Start Parsing PDF"
+        )}
+      </Button>
+      <p>Status: {status}</p>
+    </div>
   );
 }
